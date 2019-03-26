c07a0db2a2fbea0ba3854c650e523fddf3377d79
Hardik Patel <hardikgw@gmail.com>
2019-03-26 00:25:25 -0400

    split file

--- NeuralKeras.py
+++ NeuralKeras.py
@@ -15,21 +15,33 @@ def main():
 # dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
 
 def split_files():
+    import shutil
+
     path_ = '/Users/hp/workbench/projects/gmu/gmu-exam/data'
     split_len = 100
-
+    split_dir = 'split'
     import os
 
     def files(path):
         for file in os.listdir(path):
             if os.path.isfile(os.path.join(path, file)):
-                yield path + '/' + file
+                yield path, file
 
-    for f in files(path_):
+    def create_file(path, dir, filename):
+        new_folder = path + '/' + dir
+        if not os.path.exists(new_folder):
+            os.makedirs(new_folder)
+        # else:
+        #     shutil.rmtree(new_folder)
+        #     os.makedirs(new_folder)
+        return open(new_folder + '/' + filename, 'w')
+
+    for p, f in files(path_):
         ctr = 0
-        with open(f, "r") as fp:
+        new_file = create_file(p, split_dir, f)
+        with open(p + '/' + f, "r") as fp:
             for line in fp:
-                print(line)
+                new_file.write(line)
                 ctr += 1
                 if split_len < ctr:
                     break
