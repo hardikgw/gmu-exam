f856ea180e3b3b557f085d1321f4fe797c587952
Hardik Patel <hardikgw@gmail.com>
2019-03-27 11:22:57 -0400

    split file

--- NNKeras.py
+++ NNKeras.py
@@ -1,26 +1,11 @@
 import tensorflow as tf
-from tensorflow.keras import layers
+import os
 
 
-def main():
-    print('')
-
-
-# iris = datasets.load_iris()
-# x = iris.data
-# y = iris.target
-#
-# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.5)
-#
-# dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
-
-def split_files():
-    import shutil
-
+def create_dataset():
     path_ = '/Users/hp/workbench/projects/gmu/gmu-exam/data'
     split_len = 100
     split_dir = 'split'
-    import os
 
     def files(path):
         for file in os.listdir(path):
@@ -28,23 +13,43 @@ def split_files():
                 yield path, file
 
     def create_file(path, dir, filename):
-        new_folder = path + '/' + dir
+        new_folder = os.path.join(path, dir)
         if not os.path.exists(new_folder):
             os.makedirs(new_folder)
         # else:
         #     shutil.rmtree(new_folder)
         #     os.makedirs(new_folder)
-        return open(new_folder + '/' + filename, 'w')
+        return open(os.path.join(new_folder, filename), 'w')
 
-    for p, f in files(path_):
-        ctr = 0
-        new_file = create_file(p, split_dir, f)
-        with open(p + '/' + f, "r") as fp:
-            for line in fp:
-                new_file.write(line)
-                ctr += 1
-                if split_len < ctr:
-                    break
+    def join_split_files(path):
+        new_file_name = "dataset"
+        new_file = create_file(path, 'dataset', new_file_name)
+        for p, f in files(os.path.join(path, split_dir)):
+            with open(os.path.join(p, f), "r") as fp:
+                for line in fp:
+                    new_file.writelines(f + ',' + ','.join(line.split()) + '\n')
+
+    def split_files(path):
+        for p, f in files(path):
+            ctr = 0
+            new_file = create_file(p, split_dir, f)
+            with open(os.path.join(p, f), "r") as fp:
+                for line in fp:
+                    new_file.write(line)
+                    ctr += 1
+                    if split_len < ctr:
+                        break
+
+    # split_files(path_)
+    join_split_files(path_)
 
 
-split_files()
+# def train():
+
+
+def main():
+    create_dataset()
+    # train()
+
+
+main()
