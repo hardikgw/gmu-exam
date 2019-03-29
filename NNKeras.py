7d2d8cc1a856efd0a0a804e885c6de57c6a470c5
Hardik Patel <hardikgw@gmail.com>
2019-03-29 16:58:04 -0400

    dataset

--- NNKeras.py
+++ NNKeras.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from keras import Sequential
 
@@ -12,21 +13,20 @@ class NNKeras:
         self._file = file
 
     def prepare(self):
-        lables = ['R', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-        dataset = pd.read_csv(os.path.join(self._path, self._file), names=lables)
+        dataset = pd.read_csv(os.path.join(self._path, self._file), header=None)
 
-        dataset.describe()
-        print(dataset.head(2000))
-        num_classes = 10
+        X = dataset[:, 1, 3]
+        y = dataset[:, 1]
+
+        print(X)
+        print(y)
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+        # print(dataset.head(2000)
 
         model = Sequential()
-        # model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1), activation='relu', input_shape=input_shape))
-        # model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-        # model.add(Conv2D(64, (5, 5), activation='relu'))
-        # model.add(MaxPooling2D(pool_size=(2, 2)))
-        # model.add(Flatten())
-        # model.add(Dense(1000, activation='relu'))
-        # model.add(Dense(num_classes, activation='softmax'))
+        model.add(Dense(1, activation='relu', input_shape=(1,)))
 
 
 train = NNKeras("/Users/hp/workbench/projects/gmu/neural-network-poc/data/dataset", "dataset")
+train.prepare()
