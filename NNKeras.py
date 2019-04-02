3efefcd22b3601fd470eccffcdfcebd6c3300769
Hardik Patel <hardikgw@gmail.com>
2019-04-02 00:18:42 -0400

    notebook

--- NNKeras.py
+++ NNKeras.py
@@ -13,13 +13,12 @@ from keras.models import Sequential
 
 
 class NNKeras:
-    def __init__(self, path: str, file: str):
-        self._path = path
-        self._file = file
+    def __init__(self, url: str):
+        self._url = url
         self._num_cols = 64
 
     def read_data(self):
-        df = pd.read_csv(os.path.join(self._path, self._file), header=None)
+        df = pd.read_csv(self._url, header=None)
         X = df.iloc[:, 1:].astype(float)
         classes = df.iloc[:, 0]
         unique_classes = pd.DataFrame(sorted([c.upper() for c in classes.unique()]))
@@ -41,8 +40,7 @@ class NNKeras:
         model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
         return model
 
-    def train(self):
-        X, y, unique_classes = self.read_data()
+    def train(self, X, y):
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05, random_state=5)
 
         for num_nodes in range(2, 100):
@@ -53,5 +51,6 @@ class NNKeras:
             print(num_nodes, accuracy)
 
 
-nn = NNKeras("/Users/hp/workbench/projects/gmu/neural-network-poc/data/dataset", "dataset.csv")
-nn.train()
+nn = NNKeras("/Users/hp/workbench/projects/gmu/neural-network-poc/data/dataset/dataset.csv")
+X, y, unique_classes = nn.read_data()
+nn.train(X, y)
--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -it -p 8888:8888 -p 6006:6006 --name=tensorflow -v $(pwd)/notebooks:/tf/notebooks -v $(pwd)/data/dataset:/tf/dataset $(pwd)/logs:/tf/logs -v $(pwd)/models:/tf/models -e PASSWORD=password tensorflow/tensorflow:latest-py3-jupyter
+docker run -it -p 8888:8888 -p 6006:6006 --name=tensorflow -v $(pwd)/notebooks:/tf/notebooks -v $(pwd)/data/dataset:/tf/dataset -v $(pwd)/logs:/tf/logs -v $(pwd)/models:/tf/models -e PASSWORD=password tensorflow/tensorflow:latest-py3-jupyter
