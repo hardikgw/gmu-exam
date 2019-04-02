ee9e47ebce4cba8f2219def17dbb272a80096db6
Hardik Patel <hardikgw@gmail.com>
2019-04-02 00:35:00 -0400

    notebook

--- docker.sh
+++ docker.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 docker run -it -p 8888:8888 -p 6006:6006 --name=tensorflow -v $(pwd)/notebooks:/tf/notebooks -v $(pwd)/data/dataset:/tf/dataset -v $(pwd)/logs:/tf/logs -v $(pwd)/models:/tf/models -e PASSWORD=password tensorflow/tensorflow:latest-py3-jupyter
+docker exec tensorflow tensorboard --logdir=/tf/logs
