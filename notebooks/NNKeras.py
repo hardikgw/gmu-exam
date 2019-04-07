32dc7351a66bf5c9c9ffd1aa211a7d4362c5ab05
Hardik Patel <hardikgw@gmail.com>
2019-04-07 12:58:56 -0400

    code cleanup and tensorboard

--- notebooks/NNKeras.py
+++ notebooks/NNKeras.py
@@ -21,7 +21,6 @@ class NNKeras:
         self._num_classes = 31
         self._model = "../models/model.h5"
         self._log_dir = "../logs"
-        self.layer_names = ['features']
 
     def read_data(self):
         df = pd.read_csv(self._url, header=None)
@@ -36,48 +35,59 @@ class NNKeras:
             y[i, col_idx] = True
         return X, y, unique_classes
 
-    def base_model(self, nodes, num_output=31, kernel_regularizer=None):
+    def base_model(self, nodes, num_output=31, kernel_regularizer=None, layer_id=None):
         model = Sequential()
         for prev_node, node in zip(nodes[:-1], nodes[1:]):
-            print("nodes_{}_{}".format(prev_node, node))
+            layer_name = None
+            if layer_id is not None:
+                layer_name = "{}-in-{}-n-{}".format(layer_id, str(prev_node), str(prev_node))
             model.add(Dense(node, activation='relu', kernel_regularizer=kernel_regularizer,
-                            input_dim=prev_node))
+                            input_dim=prev_node, name=layer_name))
         model.add(Dense(num_output, activation='sigmoid', name='features'))
         model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
         return model
 
-    def train(self, X, y, node_range, show_summary: bool = False):
+    def train(self, X, y, show_summary: bool = False):
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01, random_state=5)
-        for num_nodes in node_range:
-            nodes = [64, num_nodes]
-            model = self.base_model(nodes)
-            summary = model.fit(X_train, y_train, epochs=10, verbose=0)
-            score = model.evaluate(X_test, y_test)
-            if show_summary:
-                model.summary()
-            print('Test loss:', score[0])
-            print('Test accuracy:', score[1])
+        nodes = [64, 6]
+        model = self.base_model(nodes)
+        summary = model.fit(X_train, y_train, epochs=10, verbose=0)
+        score = model.evaluate(X_test, y_test)
+        if show_summary:
+            model.summary()
+        print('Test loss:', score[0])
+        print('Test accuracy:', score[1])
 
     def train_with_callback(self, X, y, model_file: str = None, generate_plots=True):
         if model_file is None:
             model_file = self._model
         plot_losses = TrainingPlot()
         time_summary = TimeSummary()
+        layer_names = ['features']
+        num_nodes = 6
+        for layer_id in range(6, 7):
+            nodes = [64, num_nodes]
+            for prev_node, node in zip(nodes[:-1], nodes[1:]):
+                layer_name = None
+                if layer_id is not None:
+                    layer_name = "{}-in-{}-n-{}".format(layer_id, str(prev_node), str(prev_node))
+                    layer_names.append(layer_name)
+
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01, random_state=5)
         with open(os.path.join(self._log_dir, 'metadata.tsv'), 'w') as f:
             np.savetxt(f, y_test)
         tensorboard = TensorBoard(log_dir=self._log_dir, histogram_freq=2, batch_size=32,
                                   write_graph=True,
                                   write_grads=False, write_images=True, embeddings_freq=1,
-                                  embeddings_layer_names=self.layer_names,
+                                  embeddings_layer_names=['features'],
                                   embeddings_data=X_test,
                                   update_freq='epoch')
         callbacks = [tensorboard, time_summary]
         if generate_plots:
             callbacks.append(plot_losses)
-        for num_nodes in range(31, 32):
+        for layer in range(6, 7):
             nodes = [64, num_nodes]
-            model = self.base_model(nodes)
+            model = self.base_model(nodes, layer_id="l-{}".format(layer))
             summary = model.fit(X_train, y_train, epochs=100, verbose=0, validation_data=(X_test, y_test),
                                 callbacks=callbacks)
             score = model.evaluate(X_test, y_test)
@@ -137,9 +147,10 @@ class NNKeras:
                 if i > num_lines:
                     break
 
-# nn = NNKeras("/Users/hp/workbench/projects/gmu/neural-network-poc/data/dataset/dataset1.csv")
-# X, y, classes = nn.read_data()
-# nn.train_with_callback(X, y, "../logs/model_1.ckpt", False)
+
+nn = NNKeras("/Users/hp/workbench/projects/gmu/neural-network-poc/data/dataset/dataset1.csv")
+X, y, classes = nn.read_data()
+nn.train_with_callback(X, y, "../logs/model.ckpt", False)
 # nn.predict("/Users/hp/workbench/projects/gmu/neural-network-poc/data/fix/AE002161.csv", classes)
 # # avg_score = nn.single_output_score(X, y)
 # # print("Average accuracy:", avg_score)
